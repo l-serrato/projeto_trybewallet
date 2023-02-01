b365e701077a2086301303c3656f3343846db159
Luis <l-serrato@live.com>
2023-02-01 16:25:19 -0300

    acts

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,19 +1,8 @@
-// import user from './user';
+import user from './user';
 // import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-import { combineReducers } from 'redux';
+const reducer = combineReducers({ user });
 
-const INITIAL_STATE = {};
-
-const exampleReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-  default:
-    return state;
-  }
-};
-
-const rootReducer = combineReducers({ exampleReducer });
-
-export default rootReducer;
+export default reducer;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INICIAL_STATE = {
+  user: {
+    email: '',
+    senha: '',
+  },
+};
+
+function user(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case LOGIN:
+    return {
+      ...state,
+      email: action.user.email,
+      senha: action.user.senha,
+    };
+  default:
+    return state;
+  }
+}
+
+export default user;
