c196e30c60b1967ea5b9f11d8e3ee2d3c976caf4
Luis <l-serrato@live.com>
2023-02-03 12:33:24 -0300

    test 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,36 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+        </tr>
+        <tr>
+          <th>Tag</th>
+        </tr>
+        <tr>
+          <th>Método de pagamento</th>
+        </tr>
+        <tr>
+          <td>Valor</td>
+        </tr>
+        <tr>
+          <th>Moeda</th>
+        </tr>
+        <tr>
+          <th>Câmbio utilizado</th>
+        </tr>
+        <tr>
+          <th>Valor convertido</th>
+        </tr>
+        <tr>
+          <th>Moeda de conversão</th>
+        </tr>
+        <tr>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
+
     );
   }
 }
